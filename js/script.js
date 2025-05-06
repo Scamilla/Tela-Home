bdd19e418b120dc6c2f87d97257f5b2b8ec6f789
João Scamilla <joaoscamilla@gmail.com>
2025-05-06 16:22:20 -0300

    tela-home

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
+
+//Carrossel de Imagens
 const carrossel = document.getElementById('carrossel');
-        const imagens = carrossel.querySelectorAll('img');
-        const anterior = document.getElementById('anterior');
-        const proximo = document.getElementById('proximo');
+const imagens = carrossel.querySelectorAll('img');
+const anterior = document.getElementById('anterior');
+const proximo = document.getElementById('proximo');
     
-        let indiceAtual = 0;
+let indiceAtual = 0;
     
-        anterior.addEventListener('click', () => {
-          indiceAtual = (indiceAtual > 0) ? indiceAtual - 1 : imagens.length - 1;
-          atualizarCarrossel();
-        });
+anterior.addEventListener('click', () => {
+  indiceAtual = (indiceAtual > 0) ? indiceAtual - 1 : imagens.length - 1;
+  atualizarCarrossel();
+});
     
-        proximo.addEventListener('click', () => {
-          indiceAtual = (indiceAtual < imagens.length - 1) ? indiceAtual + 1 : 0;
-          atualizarCarrossel();
-        });
+proximo.addEventListener('click', () => {
+  indiceAtual = (indiceAtual < imagens.length - 1) ? indiceAtual + 1 : 0;
+  atualizarCarrossel();
+});
     
-        function atualizarCarrossel() {
-          carrossel.style.transform = `translateX(-${indiceAtual * 100}%)`;
-        }
+function atualizarCarrossel() {
+  carrossel.style.transform = `translateX(-${indiceAtual * 100}%)`;
+}
+
+// Menu Responsivo
+const toggleMenu = document.getElementById('toggle-menu');
+const menuBaixoLista = document.getElementById('menu-baixoListaResponsivo');
+
+toggleMenu.addEventListener('click', () => {
+    menuBaixoLista.classList.toggle('hide');
+    menuBaixoLista.classList.toggle('show');
+    toggleMenu.classList.toggle('open');
+});
